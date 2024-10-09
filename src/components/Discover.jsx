4a568bd0e8ffb0d3dee27bbd7d2a434cc53f2af7
Linus Ilbratt <linusilbratt@gmail.com>
2024-10-09 15:06:29 +0200

    header and navbar fixed. Header is fetching image and text info properly from the api

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -3,11 +3,14 @@ import Navbar from "./Navbar";
 import Header from "./Header";
 import MovieCard from "./MovieCard";
 import placeholder from "../features/placeholder";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Discover = () => {
     const ListData = placeholder.getMovieListPlaceholder();
     const [selectedMovieID, setSelectedMovieID] = useState('');
+    const [backdropUrl, setBackdropUrl] = useState(""); 
+    const [movieTitle, setMovieTitle] = useState("");
+    const [movieOverview, setMovieOverview] = useState("");
     const popularCaruselItems = []; 
 
     const handlePosterPress = (id) => {
@@ -28,22 +31,39 @@ const Discover = () => {
     }
     fillCarusell();
 
-    return (
-      <div className="body-container">
-          <div className="header-container"> {/* Ny container för headern */}
-              <Navbar />
-              <Header />
+    const fillHeader = () => {
+      if (ListData.results && ListData.results.length > 0) {
+          const firstMovie = ListData.results[0];
+          const baseImageUrl = 'https://image.tmdb.org/t/p/original';
+          setBackdropUrl(`${baseImageUrl}${firstMovie.backdrop_path}`);
+          setMovieTitle(firstMovie.title);
+          setMovieOverview(firstMovie.overview)
+      }
+  };
+  useEffect(() => {
+    fillHeader();
+}, []);
+
+return (
+  <div className="body-container">
+      <div className="header-container">
+          <Navbar />
+          <Header backdropUrl={backdropUrl} movieTitle={movieTitle} movieOverview={movieOverview} /> 
+      </div>
+
+      <div className="movie-genre-container">
+          <div className="popular-movie-container">
+              <Carusel items={popularCaruselItems} title={"Populära Filmer"} />
           </div>
-  
-          <div className="movie-genre-container">
-              <div className="popular-movie-container">
-                  <Carusel items={popularCaruselItems} title={"Populära Filmer"} />
-              </div>
-              {/* Lägg till fler genrer här, fylla på dynamiskt? */}
+          <div className="popular-movie-container">
+              <Carusel items={popularCaruselItems} title={"Populära Filmer"} />
+          </div>
+          <div className="popular-movie-container">
+              <Carusel items={popularCaruselItems} title={"Populära Filmer"} />
           </div>
       </div>
-  );
-  
+  </div>
+);
 }
 
 export default Discover;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./Header.css";
 
-const Header = () => {
+const Header = ({ backdropUrl, movieTitle, movieOverview }) => {
     return (
-        <header className="header">
+        <header className="header" style={{ backgroundImage: `url(${backdropUrl})` }}>
             <div className="header-content">
-                <h1>BAD BOYS</h1>
-                <h2>RIDE OR DIE</h2>
-                <p>
-                    När kapten Howard blir anklagad för att ha varit korrupt, lovar Mike och Marcus att rentvå hans namn. Men när de närmar sig sanningen, blir de själva anklagade och måste fly med ett pris på sina huvuden.
-                </p>
+                <h1>{movieTitle}</h1>               
+                <p>{movieOverview}</p>
             </div>
         </header>
     );

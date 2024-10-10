47a92739d4285c5a37977b00c8f19990a8982115
KT <thun.kristian@gmail.com>
2024-10-10 17:47:44 +0200

    added some more genres

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -106,11 +106,38 @@ const Discover = () => {
     }, []);
 
     const genres = [
-        { id: 28, name: "Action" },
-        { id: 35, name: "Comedy" },
-        { id: 18, name: "Drama" },
-        { id: 878, name: "Science Fiction" },
-        { id: 27, name: "Horror" }
+        {
+            "id": 28,
+            "name": "Action"
+        },
+        {
+            "id": 12,
+            "name": "Adventure"
+        },
+        {
+            "id": 16,
+            "name": "Animation"
+        },
+        {
+            "id": 35,
+            "name": "Comedy"
+        },
+        {
+            "id": 80,
+            "name": "Crime"
+        },
+        {
+            "id": 99,
+            "name": "Documentary"
+        },
+        {
+            "id": 18,
+            "name": "Drama"
+        },
+        {
+            "id": 10751,
+            "name": "Family"
+        },
     ];
 
     const fetchMovieData = async () => {

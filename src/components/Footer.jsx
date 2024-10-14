c59039d8ecaf7d0de17ea761b221ee6a0810854a
Gurkansson <fil.gustavsson@gmail.com>
2024-10-14 14:39:02 +0200

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <p>
+            
+            <p className="contact-info">Contact us: info@grupp2.com</p>
+            <p>
+            <p>&copy; 2024 ReelReactors. All rights reserved.</p>
+            <p><a href="/terms">Terms of Service</a> | <a href="/privacy">Privacy Policy</a></p>
+            <p>This website/service uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB.</p>
+            </p>
+            </p>
+        </div>
+    );
+};
+
+export default Footer;

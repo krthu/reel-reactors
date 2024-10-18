e8ac9a41570e4b2bfe2839adfa1fd602c9b83cdc
Linus Ilbratt <linusilbratt@gmail.com>
2024-10-18 11:08:39 +0200

    changed cartItem and ShoppingCart

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -13,33 +13,35 @@ const CartItem = ({ cartItem, isOrderDetails = false }) => {
     };
 
     return (
-        <div className='cart-item'>
-            <div className="cart-item-info">            
-                <h4>{cartItem.item.title}</h4>
-                <img src={`${baseImageURL}${cartItem.item.poster_path}`} alt="Produktbild" />
-                <div className="cart-item-details">                    
-                    <p><span>{cartItem.item.original_language}</span></p>
+        <div className="cart-item-container">
+            <div className="cart-item">
+                <div className="cart-item-info">            
+                    <h4>{cartItem.item.title}</h4>
+                    <img src={`${baseImageURL}${cartItem.item.poster_path}`} alt="Produktbild" />
+                    <div className="cart-item-details">                    
+                        <p><span>{cartItem.item.original_language}</span></p>
+                    </div>
                 </div>
-            </div>
 
-            
-            <div className="cart-item-count">
-                <span>{cartItem.count}</span>
-            </div>
+                <div className="cart-item-count">
+                    <span>{cartItem.count}</span>
+                </div>
 
-            <div className="cart-item-price">
-                <p>{cartItem.price}:-</p>
-            </div>
+                <div className="cart-item-price">
+                    <p>{cartItem.price}:-</p>
+                </div>
 
-            <div className="cart-item-delete"> 
-                {isOrderDetails ? null :
-                    <button className="delete-button" onClick={handleDeletePress}>
-                        <i className="fas fa-trash-alt"></i>
-                    </button>
-                }
+                <div className="cart-item-delete"> 
+                    {isOrderDetails ? null :
+                        <button className="delete-button" onClick={handleDeletePress}>
+                            <i className="fas fa-trash-alt"></i>
+                        </button>
+                    }
+                </div>
             </div>
         </div>
     );
 };
+
 
 export default CartItem;
